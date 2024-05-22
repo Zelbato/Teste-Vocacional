a3f4146cb95a1502f87f23e5cd533fc1b8349efc
Heitor Zelbato <heitorzelbato@gmail.com>
2024-05-22 19:40:40 -0300

    organizando links de acesso

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function menu(){
     let menuMobile = document.querySelector(".menu-mobile");
     if(menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "Imagens/cardapio.png";
+        document.querySelector('.icon').src = "Src/assets/Imagens/cardapio.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector(".icon").src = "Imagens/botao-excluir.png";
+        document.querySelector(".icon").src = "Src/assets/Imagens/botao-excluir.png";
     }
 }
 
