2e8b7e96a1408cc49ad477c4402398b824d6238d
Heitor Zelbato <heitorzelbato@gmail.com>
2024-06-19 16:06:34 -0300

    arrumando erros

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-function menu(){
+function menu() {
 
     let menuMobile = document.querySelector(".menu-mobile");
-    if(menuMobile.classList.contains('open')) {
+    if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "Src/assets/Imagens/cardapio.png";
     } else {
@@ -15,9 +15,14 @@ let boxBusca = document.querySelector('.busca');
 let lupa = document.querySelector('.lupa-buscar');
 let btnFechar = document.querySelector('.btn-fechar');
 
-lupa.addEventListener('click', ()=> {
-   boxBusca.classList.add('ativar')
+lupa.addEventListener('click', () => {
+    boxBusca.classList.add('ativar')
 
 })
 
 
+//muda cor do menu
+window.addEventListener('scroll', function (){
+    let header = document.querySelector('#header');
+    header.classList.toggle('rolagem',window.scrollY > 1);
+})
