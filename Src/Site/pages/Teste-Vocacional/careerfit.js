a75ecbfdbbe8011cf1f0f1e86f8cda5ec9b37b77
Zelbato <heitorzelbato@gmail.com>
2024-06-27 10:46:01 -0300

    Criando o Teste-Vocacional pt-2

---
+++ Src/Site/pages/Teste-Vocacional/careerfit.js
@@ -0,0 +1,113 @@
+
+const main = document.querySelector('.main');
+const quizSelection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
+
+
+let quesCount = 0;
+let quesNumb = 1;
+let userScore = 0;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {//Proxima questão
+    if (quesCount < questions.length - 1) {
+        quesCount++;
+        showQuestions(quesCount);
+
+        quesNumb++;
+        questCounter(quesNumb);
+
+        nextBtn.classList.remove('active');
+
+    } else {
+        showResultBox();
+    }
+
+}
+
+const optionList = document.querySelector('.option-list');
+
+optionList.onclick = () => {//Selecionar a opção
+
+}
+
+//Recebendo as questôes e opções em array
+
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML = optionTag;
+
+    const option = document.querySelectorAll('.option');
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute('onclick', 'optionSelected(this)');
+    }
+}
+
+function optionSelected(answer) {
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[quesCount].answer;
+    let allOptions = optionList.children.length;
+
+    if (userAnswer == correctAnswer) {
+        answer.classList.add('correct');
+        userScore += 1;
+        headerScore();
+
+    } else {
+        answer.classList.add('incorrect');
+
+        //se a resposta estiver incorreta, selecione automaticamente a resposta correta
+
+        for (let i = 0; i < allOptions; i++) {
+            if (optionList.children[i].textContent == correctAnswer) {
+                optionList.children[i].setAttribute('class', 'option correct');
+            }
+        }
+
+
+    }
+
+    //se o usuário selecionou outra opção, desativou todas as opções
+
+    for (let i = 0; i < allOptions; i++) {
+        optionList.children[i].classList.add('disabled');
+    }
+
+    nextBtn.classList.add('active');
+}
+
+function questCounter(index) {
+    const questTotal = document.querySelector('.question-total');
+    questTotal.textContent = `${index} de ${questions.length} Questôes`;
+}
+
+
+function showResultBox() {
+    quizBox.classList.remove('active');
+    resultBox.classList.add('active');
+
+    let progressStartValue = -1;
+    let progressEndValue = (userScore / questions.length) * 100;
+    let speed = 20;
+
+    let progress = setInterval(() => {
+        progressStartValue++;
+
+        progressValue.textContent = `${progressStartValue}%`;
+        circuloProgress.style.background = `conic-gradient(#355899 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
+
+        if(progressStartValue == progressEndValue) {
+            clearInterval(progress);
+        }
+       
+    }, speed);
+}
---
+++ Src/Site/pages/Teste-Vocacional/questCareerFit.js
@@ -0,0 +1,63 @@
+let questions = [
+    {
+        numb: 1,
+        question:"Quais dessas palavras nos usamos em um aeroporto? ",
+        answer:"C. Airplane, Airport, baggage, fligth ticket and board",
+        options: [
+            "A.  Visa, airport, board, Gate, hand ladder",
+            "B. Gate, airport, airplane, foot and retired",
+            "C. Airplane, Airport, baggage, fligth ticket and board",
+            "D. Check-in, Gate, Visa, hand ladder and pocket watch"
+        ]
+    },
+
+    {
+        numb: 2,
+        question:"Hello Paulo, what would you like to eat tomorrow morning? Esta frase está",
+        answer:"D. Futuro",
+        options: [
+            "A.  Presente",
+            "B. Passado",
+            "C. Futuro do pretérito.",
+            "D. Futuro"
+        ]
+    },
+
+    {
+        numb: 3,
+        question:"Quais dessas palavras estão todas no passado(simple past)?",
+        answer:"A. Died, Cried, Enjoyed, Invited and Worried ",
+        options: [
+            "A. Died, Cried, Enjoyed, Invited and Worried ",
+            "B. Answered, Died, Awakened, Failed and soon ",
+            "C.Tomorrow, cried, died, enjoyed and worried ",
+            "D. Soon, Tomorrow, cried, failed and enjoyed "
+        ]
+    },
+    {
+        numb: 4,
+        question:"Spring begins on March 20th, June 20ht or 21st, in the United States, qual é a estação? ",
+        answer: "C. primavera ",
+        options: [
+            "A. outono",
+            "B. verão ",
+            "C. primavera ",
+            "D. inverno "
+        ]
+    },
+
+    {
+        numb: 5,
+        question:"At the group of words below, mark thye one that does not include any Irregular verbs?",
+        answer: "A. Kicked, killed, kissed, moved, murdered ",
+        options: [
+            "A. Kicked, killed, kissed, moved, murdered ",
+            "B. Kicked, forgave, kissed, traveled, died ",
+            "C. Began, kissed, died, caught, bougth ",
+            "D. Fed, died, forgotten, bit, kissed"
+        ]
+    
+    }
+
+]
+
