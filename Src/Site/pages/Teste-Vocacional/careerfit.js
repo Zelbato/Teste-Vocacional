15e5faa88afe926e9de580fc38623f8303f3578f
Heitor Zelbato <heitorzelbato@gmail.com>
2024-06-27 22:09:44 -0300

    Alterando nome do site e responsividade

--- Src/Site/pages/Teste-Vocacional/careerfit.js
+++ Src/Site/pages/Teste-Vocacional/careerfit.js
@@ -1,4 +1,35 @@
 
+
+
+//muda cor do menu
+window.addEventListener('scroll', function (){
+    let header = document.querySelector('.header');
+    header.classList.toggle('rolagem',window.scrollY > 150);
+})
+
+
+function menu(){
+
+    let menuMobile = document.querySelector(".menu-mobile");
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "/Src/assets/Imagens/cardapio.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector(".icon").src = "/Src/assets/Imagens/botao-excluir.png";
+    }
+}
+
+let boxBusca = document.querySelector('.busca');
+let lupa = document.querySelector('.lupa-buscar');
+let btnFechar = document.querySelector('.btn-fechar');
+
+lupa.addEventListener('click', ()=> {
+   boxBusca.classList.add('ativar')
+
+})
+
+
 const main = document.querySelector('.main');
 const quizSelection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
