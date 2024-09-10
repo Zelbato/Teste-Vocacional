5850c5dd89573cdd899e241c5fa68c25a9391fe6
Heitor Zelbato <heitorzelbato@gmail.com>
2024-09-09 22:06:45 -0300

    criando cards para adm

--- Src/assets/js/index_adm.js
+++ Src/assets/js/index_adm.js
@@ -1,4 +1,44 @@
 
+
+const initTypingAnimation = () => {
+  const title = document.querySelector('.cards .card-titulo h1')
+  const span = document.querySelector('.cards .card-titulo .sub-title')
+
+  const typingAnimation = (element) => {
+
+      if (element == title) {
+          element.innerHTML = 'Bem Vindo devolta,  '
+          const textToArray = element.innerHTML.split('')
+          element.innerHTML = ''
+
+          textToArray.forEach((item, index) => {
+              setTimeout(() => element.innerHTML += item, 120 * index)
+          })
+
+      } else if (element == span) {
+          element.innerHTML = 'Área do Administrador! \n '
+          const textToArray = element.innerHTML.split('')
+          element.innerHTML = ''
+
+          textToArray.forEach((item, index) => {
+              setTimeout(() => element.innerHTML += item, 150 * index)
+          })
+
+      }
+
+  }
+
+  typingAnimation(title)
+  setTimeout(() => typingAnimation(span), 1600)
+
+}
+
+
+
+initTypingAnimation()
+
+
+
 const tooltip = document.querySelector(".tooltip");
 const menuButton = document.querySelector(".menu-button");
 
