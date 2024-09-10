290a5aafbb1e5f632c53c4c156b17a84911d9cc2
Heitor Zelbato <heitorzelbato@gmail.com>
2024-09-09 22:48:44 -0300

    parte ADM mobile

--- Src/assets/js/index_adm.js
+++ Src/assets/js/index_adm.js
@@ -99,3 +99,15 @@ restart_quiz.onclick = () => {
   quit_quiz.onclick = () => {
     window.location.reload(); //reload the current window
   };
+
+  function menu() {
+
+    let menuMobile = document.querySelector(".menu-mobile");
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "/Src/assets/Imagens/cardapio.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector(".icon").src = "/Src/assets/Imagens/botao-excluir.png";
+    }
+}
