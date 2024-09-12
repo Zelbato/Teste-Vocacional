4ee85cf4aacf783f0e638f445053e32d96dc9833
Heitor Zelbato <heitorzelbato@gmail.com>
2024-09-12 16:35:36 -0300

    Organizando pastas (Finalizando pagina principal da Instituição)

--- Src/Public/assets/Js/cadastro.js
+++ Src/Public/assets/Js/cadastro.js
@@ -19,10 +19,10 @@ function menu(){
     let menuMobile = document.querySelector(".menu-mobile");
     if(menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "/Src/assets/Imagens/cardapio.png";
+        document.querySelector('.icon').src = "/Src/Public/assets/Img/cardapio.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector(".icon").src = "/Src/assets/Imagens/botao-excluir.png";
+        document.querySelector(".icon").src = "/Src/Public/assets/Img/botao-excluir.png";
     }
 }
 
