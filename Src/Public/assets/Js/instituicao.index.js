4a04d0910a3bf6e6fe301537e0e74e1de1187325
Heitor Zelbato <heitorzelbato@gmail.com>
2024-09-14 16:53:29 -0300

    arrumando erros

---
+++ Src/Public/assets/Js/instituicao.index.js
@@ -0,0 +1,58 @@
+
+
+const initTypingAnimation = () => {
+    const title = document.querySelector('.cards .card-titulo h1')
+    const span = document.querySelector('.cards .card-titulo .sub-title')
+  
+    const typingAnimation = (element) => {
+  
+        if (element == title) {
+            element.innerHTML = 'Seja bem vindo,  '
+            const textToArray = element.innerHTML.split('')
+            element.innerHTML = ''
+  
+            textToArray.forEach((item, index) => {
+                setTimeout(() => element.innerHTML += item, 120 * index)
+            })
+  
+        } else if (element == span) {
+            element.innerHTML = 'área da Instituição! \n '
+            const textToArray = element.innerHTML.split('')
+            element.innerHTML = ''
+  
+            textToArray.forEach((item, index) => {
+                setTimeout(() => element.innerHTML += item, 150 * index)
+            })
+  
+        }
+  
+    }
+  
+    typingAnimation(title)
+    setTimeout(() => typingAnimation(span), 1600)
+  
+  }
+  
+  initTypingAnimation()
+
+
+const tooltip = document.querySelector(".tooltip");
+const menuButton = document.querySelector(".menu-button");
+
+menuButton.addEventListener("click", () => {
+    tooltip.classList.toggle("active");
+    alternarMenuButtonIcon();
+})
+function menu() {
+
+    let menuMobile = document.querySelector(".menu-mobile");
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "/Src/Public/assets/Img/cardapio.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector(".icon").src = "/Src/Public/assets/Img/botao-excluir.png";
+    }
+}
+
+
