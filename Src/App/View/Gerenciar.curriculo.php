3d88bb1ceed3eef0f0fc6e5494c80e06d4230d9c
Heitor Zelbato <heitorzelbato@gmail.com>
2024-10-17 10:48:45 -0300

    Finalizando Responsivo

--- Src/App/View/Gerenciar.curriculo.php
+++ Src/App/View/Gerenciar.curriculo.php
@@ -20,7 +20,7 @@ if (!$resul) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../../Public/assets/styles/curriculo/meuCurriculo.css">
+    <link rel="stylesheet" type="text/css" href="../../Public/assets/styles/curriculo/meuCurriculo.css?v=<?php echo time(); ?>">
 
     <!--Icones Bootstrap-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
